99b5e241fefd1de2f6529b0b8db2fe423f4bd372
NullRoot <Luis.escare@virginiogomez.cl>
2025-11-14 19:38:17 -0300

    Inicio del nuevo proyecto, se avanza en el menu principal, efectos visuales y de transicion

---
+++ script.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById("hamburgerBtn");
+const menu = document.getElementById("menu");
+const overlay = document.getElementById("overlay");
+
+btn.addEventListener('click', () => {
+    menu.classList.toggle('active'); // Alterna la clase 'active' en el side-menu
+    overlay.style.display = menu.classList.contains('active') ? 'block' : 'none';
+});
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('active');
+    overlay.style.display = 'none';
+});
+
+
